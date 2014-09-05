55d74f80600de6d5c32c2a7e4a33e5823f39b755
Pieter Vanderwerff <me@pieter.io>
2014-09-06 10:43:49 +1200

    Expose custom Map propType validators

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 "use strict";
 
+var merge = require('react/lib/merge');
 var ReactDefaultInjection = require('./src/ui/ReactDefaultInjection');
 var ReactMapComponents = require('./src/ReactMapComponents');
+var MapPropTypes = require('./src/ui/MapPropTypes');
 
 ReactDefaultInjection.inject();
 
-module.exports = ReactMapComponents;
+module.exports = merge(
+  ReactMapComponents,
+  {
+    PropTypes: MapPropTypes
+  }
+);
