a2b6cbe74524b6dc922de9046169c316c839ff1e
Pieter Vanderwerff <me@pieter.io>
2014-09-14 21:27:27 +1200

    Added Frag component
    Frag is a helper component for creating reusable React GoogleMap components. This
    component has no functionality or output, it just allows you to return multiple
    components from a custom components `render` function.
    
    The only prop required is the `map` prop (the GoogleMap instance), this is
    automatically passed down to all direct children of the `Map` component but
    will need to be manually set if you use `Frag` inside a custom component. The
    `Frag` component will then pass the `map` prop down to all of its direct children.

---
+++ src/ui/components/__tests__/ReactFrag-test.js
@@ -0,0 +1,36 @@
+/** @jsx React.DOM */
+
+jest.dontMock('../ReactFrag');
+
+describe('ReactFrag', function() {
+  it('should pass all props to all valid children', function() {
+    var React = require('react');
+    var ReactTestUtils = require('react/lib/ReactTestUtils');
+    var ReactFrag = require('../ReactFrag');
+
+    var didMount = jest.genMockFn();
+    var Test = React.createClass({
+      render: function() { return null; },
+      componentDidMount: function() {
+        didMount(this.props);
+      }
+    });
+
+    var map = {};
+    var testProp = {};
+    ReactTestUtils.renderIntoDocument(
+      <ReactFrag map={map} testProp={testProp}>
+        <Test />
+        {null}
+        {false}
+        <ReactFrag>
+          <Test />
+        </ReactFrag>
+      </ReactFrag>
+    );
+
+    expect(didMount.mock.calls.length).toBe(2);
+    expect(didMount.mock.calls[0][0]).toEqual({map: map});
+    expect(didMount.mock.calls[1][0]).toEqual({map: map});
+  });
+});
